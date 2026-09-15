55cbc7453c318b8f21b17c6e6e11812388ed59ef
Hugo Cabral Reis <hugo33307166@edu.df.senac.br>
2026-09-15 19:41:34 -0300

    agora com form arrumado

--- landing-page/src/components/Card.jsx
+++ landing-page/src/components/Card.jsx
@@ -1,13 +1,13 @@
-function Card({titulo,children}){
-    return(
-        <div className="card">
-            <h3>{titulo}</h3>
+function Card({ titulo, children }) {
+  return (
+    <div className="card">
+      <h3>{titulo}</h3>
 
-            <div className="card-conteudo">
-                {children}
-            </div>
+      <div className="card-conteudo">
+        {children}
         </div>
-    )
+    </div>
+  );
 }
 
 export default Card;
--- landing-page/src/components/Contato.jsx
+++ landing-page/src/components/Contato.jsx
@@ -1,51 +1,49 @@
 import Input from "./Input";
 
-function Contato(){
-    return(
-        <section className="contato">
-            <h2>Entre em Contato</h2>
-        <form>
-            <Input
-            label="Nome"
-            type = "text"
-            id= 'nome'
-            placeholder = 'Digite seu nome'
-            />
+function Contato() {
+  return (
+    <section className="entre-contato">
+      <h2>Entre em Contato</h2>
+      <form>
+        <Input
+          label="Nome"
+          type="text"
+          id="nome"
+          placeholder="Digite seu nome"
+        />
 
-            <Input
-            label="Email"
-            type = "email"
-            id= 'email'
-            placeholder = 'Digite seu e-mail'
-            />
+        <Input
+          label="Email"
+          type="email"
+          id="email"
+          placeholder="Digite seu e-mail"
+        />
 
-            <Input
-            label="Assunto"
-            type = "text"
-            id= 'Assunto'
-            placeholder = 'Digite o assunto '
-            />
+        <Input
+          label="Assunto"
+          type="text"
+          id="Assunto"
+          placeholder="Digite o assunto "
+        />
 
-            <Input
-            label="Telefone"
-            type = "number"
-            id= 'telefone'
-            placeholder = '(XX) 9 XXXX-XXXX'
-            />    
+        <Input
+          label="Telefone"
+          type="number"
+          id="telefone"
+          placeholder="(XX) 9 XXXX-XXXX"
+        />
 
-            <Input
-            label="Cidade"
-            type = "text"
-            id= 'cidade'
-            placeholder = 'Ex: Brasília'
-            />         
+        <Input
+          label="Cidade"
+          type="text"
+          id="cidade"
+          placeholder="Ex: Brasília"
+        />
 
-        <button>Enviar mensagem</button> 
-        </form>
-        </section>
-
-        
-    )
+        <button>Enviar mensagem</button>
+      </form>
+    </section>
+  );
 }
 
 export default Contato;
